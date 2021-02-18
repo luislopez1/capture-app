f97695d1b395c97e065c6d92c0d0d8ee6d43ddbb
Luis Lopez <lalelopez2003@utexas.edu>
2021-02-17 19:55:33 -0600

    styling

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,29 +1,41 @@
 import React from "react";
 import home1 from '../img/home1.png';
 import FaqSection from '../components/FaqSection';
+import styled from 'styled-components';
+import {About, Description, Image, Hide} from "../styles"
 
 
 const AboutSection = () => {
     return(
-        <div className = "description">
-            <div className = "title">
-                <div className="hide">
-                    <h2> we work to make</h2>
+        <About>
+            <Description>
+                <div className="title">
+                    <Hide>
+                        <h2> we work to make</h2>
+                    </Hide>
+                    <Hide>
+                        <h2> 
+                            your <span>dreams</span> come
+                        </h2>
+                    </Hide>
+                    <Hide>
+                        <h2> true.</h2>
+                    </Hide>
                 </div>
-                <div className="hide">
-                    <h2> your <span>dreams</span> come</h2>
-                </div>
-                <div className="hide">
-                    <h2> true.</h2>
-                </div>
-                <p> Contact us for any photography ideas that you have. We have professionals with amazing skills.</p>
+                <p> 
+                    Contact us for any photography ideas that you have. We have professionals with amazing skills.
+                </p>
                 <button>Contact us</button>
-            </div>
-            <div className="image">
-                <img src={home1} alt="guy with a camera" />
-            </div>
-        </div>
+            </Description>
+                <Image>
+                    <img src={home1} alt="guy with a camera" />
+                </Image>
+        </About>
     );
 };
+
+
+
+
 
 export default AboutSection;
--- src/components/ServicesSection.js
+++ src/components/ServicesSection.js
@@ -4,50 +4,85 @@ import diaphragm from '../img/diaphragm.svg';
 import money from '../img/money.svg';
 import teamwork from '../img/teamwork.svg';
 import home2 from '../img/home2.png';
+import {About, Description, Image, Hide} from "../styles"
+import styled from 'styled-components';
 
 
 
 const ServicesSection = () => {
     return(
-        <div className="services">
-            <h2> High <span>quality</span> services</h2>
-            <div className="cards">
-                <div className="card">
-                    <div className="icon">
-                        <img src={clock}/>
-                        <h3>Efficient</h3>
-                    </div>
-                    <p> Lorem ipsum dolor sit amet.</p>
-                </div>
-                <div className="card">
-                    <div className="icon">
-                        <img src={teamwork}/>
-                        <h3>Teamwork</h3>
-                    </div>
-                    <p> Lorem ipsum dolor sit amet.</p>
-                </div>
-                <div className="card">
-                    <div className="icon">
-                        <img src={diaphragm}/>
-                        <h3>diaphragm</h3>
-                    </div>
-                    <p> Lorem ipsum dolor sit amet.</p>
-                </div>
-                <div className="card">
-                    <div className="icon">
-                        <img src={money}/>
-                        <h3>Affordable</h3>
-                    </div>
-                    <p> Lorem ipsum dolor sit amet.</p>
-                </div>
-            </div>
-            <div className="image">
-                <img src={home2}/>
-            </div>
+        <Services>
+            <Description>
+                <h2> High <span>quality</span> services</h2>
+                <Cards>
+                    <Card>
+                        <div className="icon">
+                            <img src={clock}/>
+                            <h3>Efficient</h3>
+                        </div>
+                        <p> Lorem ipsum dolor sit amet.</p>
+                    </Card>
+                    <Card>
+                        <div className="icon">
+                            <img src={teamwork}/>
+                            <h3>Teamwork</h3>
+                        </div>
+                        <p> Lorem ipsum dolor sit amet.</p>
+                    </Card>
+                    <Card>
+                        <div className="icon">
+                            <img src={diaphragm}/>
+                            <h3>diaphragm</h3>
+                        </div>
+                        <p> Lorem ipsum dolor sit amet.</p>
+                    </Card>
+                    <Card>
+                        <div className="icon">
+                            <img src={money}/>
+                            <h3>Affordable</h3>
+                        </div>
+                        <p> Lorem ipsum dolor sit amet.</p>
+                    </Card>
+                </Cards>
+            </Description>
+                <Image>
+                    <img src={home2}/>
+                </Image>
 
-        </div>
+        </Services>
 
     )
 }
 
+const Services = styled(About)`
+
+    background: #181717;
+    h2{
+        padding-bottom:5rem;
+
+    }
+    p{
+        width:70%;
+        padding: 2rem 0rem 4rem 0rem;
+    }
+`;
+
+const Cards = styled.div`
+    display:flex;
+    flex-wrap:wrap;
+`;
+
+const Card = styled.div`
+    flex-basis:20;
+    .icon{
+        display:flex;
+        align-items:center;
+        h3{
+            margin-left:1rem;
+            background:white;
+            color:black;
+            padding:1rem;
+        }
+    }
+`;
 export default ServicesSection;
